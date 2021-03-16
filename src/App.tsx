76b03b2d0bcc69a55468b13f13a8e287204e8d6c
bbumjun <bumjun7928@gmail.com>
2021-03-16 21:20:00 +0900

    Chore: App 컴포넌트 위치변경

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 export const App = () => {
     return (
         <div>
-            <h1>This is TMDB movie app</h1>
+            <h1>This is TMDB movie app!</h1>
         </div>
     );
 };
