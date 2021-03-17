28077b3b9eeb136850252175d4c80b1e1b18b8c0
bbumjun <bumjun7928@gmail.com>
2021-03-18 02:33:23 +0900

    Chore: eslint 설정

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint",
+        "prettier"
+    ],
+    "extends": ["eslint:recommended", "plugin:prettier/recommended", "airbnb"],
+    "rules": {
+        "prettier/prettier": "error",
+        "semi":"error"
+    }
+};
