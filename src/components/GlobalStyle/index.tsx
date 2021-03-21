4054ba6f9d7f77bfff4c8f6f3e485effef34ba02
bbumjun <bumjun7928@gmail.com>
2021-03-22 01:07:40 +0900

    Style: global font 적용

--- src/components/GlobalStyle/index.tsx
+++ src/components/GlobalStyle/index.tsx
@@ -6,6 +6,8 @@ const GlobalStyle = createGlobalStyle`
   ${({ theme }) => `
   html,body {
     min-height:100vh;
+    font-family: 'Noto Sans KR', sans-serif;
+    font-family: 'Oxygen', sans-serif;
   }
   `}
 `;
