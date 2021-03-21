c9b0a1550f3dee9035314cbeefdbcc318e3ef9b7
bbumjun <bumjun7928@gmail.com>
2021-03-22 01:27:31 +0900

    Style: Card Image component 정적 버전 구현 및 테스트 작성

--- src/components/Card/Image.tsx
+++ src/components/Card/Image.tsx
@@ -3,15 +3,25 @@ import React from "react";
 
 interface ImageProps {
   posterPath: string;
+  title: string;
 }
 
-const Wrapper = styled.div``;
-const Img = styled.img``;
-const Image: React.FC<ImageProps> = ({ posterPath }) => {
+const Wrapper = styled.div`
+  overflow: hidden;
+`;
+const Img = styled.img`
+  width: 100%;
+`;
+const Image: React.FC<ImageProps> = ({ posterPath, title }) => {
   const imagePath = "https://image.tmdb.org/t/p/w500";
   return (
-    <Wrapper>
-      <Img src={imagePath + posterPath} />
+    <Wrapper className="max-w-xs">
+      <Img
+        className="rounded"
+        role="posterImage"
+        src={imagePath + posterPath}
+        alt={title}
+      />
     </Wrapper>
   );
 };
