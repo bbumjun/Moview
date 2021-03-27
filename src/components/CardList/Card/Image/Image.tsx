1511147ac29a62c3799cc30cf16fdea5ddf76b1c
bbumjun <bumjun7928@gmail.com>
2021-03-28 03:07:26 +0900

    Add Image atom

---
+++ src/components/CardList/Card/Image/Image.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import React from "react";
+
+interface ImageProps {
+  posterPath: string;
+  title: string;
+}
+
+const Wrapper = styled.div`
+  overflow: hidden;
+`;
+const PosterImg = styled.img`
+  width: 100%;
+`;
+const Image: React.FC<ImageProps> = ({ posterPath, title }) => {
+  const imagePath = "https://image.tmdb.org/t/p/w500";
+  return (
+    <Wrapper className="max-w-xs">
+      <PosterImg
+        className="rounded"
+        role="posterImage"
+        src={imagePath + posterPath}
+        alt={title}
+      />
+    </Wrapper>
+  );
+};
+export default Image;
