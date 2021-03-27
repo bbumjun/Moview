b418432e882764a3552495fbb9af56e20d64ca9f
bbumjun <bumjun7928@gmail.com>
2021-03-28 03:05:59 +0900

    Style: delete unused components

--- src/components/Container/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  margin: 0 auto;
-`;
-
-export default Container;
--- src/components/Text/index.tsx
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-const Text = styled.div``;
-
-export default Text;
