781fd383be553a9c06ec6bdb6e3ce36a7f548f13
bbumjun <bumjun7928@gmail.com>
2021-04-02 22:41:39 +0900

    Add AspectRatio component

---
+++ src/components/atoms/AspectRatio/index.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import styled from "styled-components";
+const Outer = styled.div`
+  width: 100%;
+  position: relative;
+  padding-top: ${(p: { ratio: number }) => `${(1 / p.ratio) * 100}%`};
+`;
+
+const Inner = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+`;
+export interface AspectRatioProps {
+  children: React.ReactNode;
+  ratio?: number;
+}
+const AspectRatio: React.FC<AspectRatioProps> = ({ children, ratio = 1 }) => (
+  <Outer ratio={ratio}>
+    <Inner>{children}</Inner>
+  </Outer>
+);
+
+export default AspectRatio;
