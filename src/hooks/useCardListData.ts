3a440e2c7b148e6772e336b682e3f256ff068907
bbumjun <bumjun7928@gmail.com>
2021-04-13 23:13:23 +0900

    Feat: CardListData fetch hook

--- src/hooks/useCardListData.ts
+++ src/hooks/useCardListData.ts
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { CardProps } from "components/molcules/Card";
 export type paramsType = {
-  language: string;
+  // language: string;
   sort_by?: string;
   with_original_language?: string;
   page?: number;
@@ -11,7 +12,6 @@ export type paramsType = {
 export interface props {
   category: string;
   params: paramsType;
-  contentTitle: string;
 }
 
 interface fetchDataTypes {
@@ -24,12 +24,12 @@ interface fetchDataTypes {
   vote_count: number;
   id: number;
 }
-const useCardListData = ({ category, params, contentTitle }: props) => {
-  const [data, setData] = useState(null);
+const useCardListData = ({ category, params }: props) => {
+  const [data, setData] = useState<CardProps[]>(null);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
   const API_KEY = process.env.REACT_APP_TMDB_KEY;
   useEffect(() => {
+    let mounted = true;
     setLoading(true);
     axios({
       method: "get",
@@ -37,40 +37,51 @@ const useCardListData = ({ category, params, contentTitle }: props) => {
       url: category,
       params: {
         api_key: API_KEY,
+        language: "ko-KR",
         region: "kr",
         include_adult: false,
         ...params,
       },
     })
       .then(({ data }) => {
-        const { results } = data;
-        const cards = results.map(
-          ({
-            poster_path,
-            release_date,
-            first_air_date,
-            title,
-            name,
-            vote_average,
-            vote_count: voteCount,
-            id,
-          }: fetchDataTypes) => ({
-            imgSrc: "https://image.tmdb.org/t/p/w500" + poster_path,
-            id,
-            title: title ?? name,
-            date: release_date ?? first_air_date,
-            rate: (Math.round((vote_average * 10) / 2) / 10).toFixed(1),
-            voteCount,
-          })
-        );
-        setData(cards);
+        if (data && mounted) {
+          const { results } = data;
+          const cards = results.map(
+            ({
+              poster_path,
+              release_date,
+              first_air_date,
+              title,
+              name,
+              vote_average,
+              vote_count: voteCount,
+              id,
+            }: fetchDataTypes) => ({
+              imgSrc: "https://image.tmdb.org/t/p/w500" + poster_path,
+              id,
+              title: title ?? name,
+              date: release_date ?? first_air_date,
+              rate: (Math.round((vote_average * 10) / 2) / 10).toFixed(1),
+              voteCount,
+            })
+          );
+          setData(cards);
+        }
       })
-      .catch((error) => setError(error))
+      .catch((err) => {
+        console.log(err);
+      })
       .finally(() => {
-        setLoading(false);
+        if (mounted) {
+          setLoading(false);
+        }
       });
+
+    return () => {
+      mounted = false;
+    };
   }, [category]);
-  return { data, loading, error, contentTitle };
+  return { data, loading };
 };
 
 export default useCardListData;
