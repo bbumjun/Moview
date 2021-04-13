c1ca3a5d05dc22b770f6544550b2530cd57b74bd
bbumjun <bumjun7928@gmail.com>
2021-04-14 00:51:14 +0900

    Feat: main template component

---
+++ src/templates/MainTemplate/style.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+export const Header = styled.header`
+  position: sticky;
+  top: 0;
+  width: 100%;
+  z-index: 999;
+`;
+export const Content = styled.section`
+  width: 100%;
+  max-width: ${({ theme }) => theme.maxWidth};
+  padding: 2rem 0;
+  margin: 0 auto;
+
+  @media (max-width: ${({ theme }) => theme.deviceSizes.laptop}) {
+    padding: 3rem;
+  }
+`;
+export const Footer = styled.footer`
+  width: 100%;
+  max-width: ${({ theme }) => theme.maxWidth};
+  margin: 0 auto;
+`;
