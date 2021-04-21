c918a510a6e22738a394beea39913833fb51b0d8
bbumjun <bumjun7928@gmail.com>
2021-04-21 17:21:14 +0900

    Move common modules

---
+++ src/common/url.ts
@@ -0,0 +1,9 @@
+// tmdb base url
+export const baseUrl = "https://api.themoviedb.org/3/";
+
+// tmdb image url
+export const smallImgUrl = "https://image.tmdb.org/t/p/w500";
+export const originalImgUrl = "https://image.tmdb.org/t/p/original";
+
+// tmdb data request url
+export const discoverUrl = "https://api.themoviedb.org/3/discover/";
