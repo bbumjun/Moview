145986d5a718b9e3a67ca9bdc595f05199d8ec4d
bbumjun <bumjun7928@gmail.com>
2021-04-28 03:18:02 +0900

    Refactor : get bgColor from props

--- src/components/molcules/BackgroundPoster/index.tsx
+++ src/components/molcules/BackgroundPoster/index.tsx
@@ -5,24 +5,21 @@ import { smallImgUrl, originalImgUrl } from "common/url";
 import { ratio } from "common/utils";
 export interface IBackgroundPoster {
   imageSrc: string;
+  bgColor: string;
 }
 
-const BackgrounPoster: React.FC<IBackgroundPoster> = ({ imageSrc }) => {
-  const [bgColor, setBgColor] = useState("");
-  getDominantColor(smallImgUrl + imageSrc).then((color) => {
-    setBgColor(color);
-    console.log(color);
-  });
-  return (
-    <S.Wrapper bgColor={bgColor}>
-      <S.ImageWrapper>
-        <S.StyledAspectRatio ratio={ratio.widescreen}>
-          <S.BgImg src={originalImgUrl + imageSrc} role={"background"} />
-          <S.Filter color={bgColor} />
-        </S.StyledAspectRatio>
-      </S.ImageWrapper>
-    </S.Wrapper>
-  );
-};
+const BackgrounPoster: React.FC<IBackgroundPoster> = ({
+  imageSrc,
+  bgColor,
+}) => (
+  <S.Wrapper bgColor={bgColor}>
+    <S.ImageWrapper>
+      <S.StyledAspectRatio ratio={ratio.widescreen}>
+        <S.BgImg src={originalImgUrl + imageSrc} role={"background"} />
+        <S.Filter color={bgColor} />
+      </S.StyledAspectRatio>
+    </S.ImageWrapper>
+  </S.Wrapper>
+);
 
 export default BackgrounPoster;
