b90e71207f37e00ea7fb18ca1ec7cab1747878f6
bbumjun <bumjun7928@gmail.com>
2021-04-30 14:11:07 +0900

    fix: contentList test component props 수정

--- src/components/organizms/ContentList/index.test.js
+++ src/components/organizms/ContentList/index.test.js
@@ -1,55 +1,51 @@
 import React from "react";
 import { render, fireEvent, waitFor, cleanup } from "test-utils";
-import CardList from "components/organizms/CardList";
-import { MemoryRouter } from "react-router-dom";
-
+import ContentList from "components/organizms/ContentList";
+import contentsData from "fakeData/movieList";
 afterEach(cleanup);
-describe("CardList", () => {
-  const category = "movie";
-  const params = {
-    sort_by: "popularity.desc",
-    with_original_language: "ko",
-  };
+describe("ContentList", () => {
+  const conetntType = "movie";
   const contentTitle = "인기있는 한국 영화";
+  const contents = contentsData.results;
 
-  it("should matches snapshot", async () => {
+  it("should matches snapshot", () => {
     const { container } = render(
-      <CardList
-        category={category}
-        params={params}
+      <ContentList
+        conetntType={conetntType}
+        contents={contents}
         contentTitle={contentTitle}
       />
     );
 
-    await waitFor(() => expect(container).toMatchSnapshot());
+    expect(container).toMatchSnapshot();
   });
-  it("renders CardList component", async () => {
-    const { findByText, debug } = render(
-      <CardList
-        category={category}
-        params={params}
+  it("renders ContentList component", () => {
+    const { getByText, debug } = render(
+      <ContentList
+        conetntType={conetntType}
+        contents={contents}
         contentTitle={contentTitle}
       />
     );
-    await findByText(contentTitle);
-    await findByText("승리호");
-    await findByText("2021.02.05");
+    getByText(contentTitle);
+    getByText("승리호");
+    getByText("2021.02.05");
   });
 
-  it("Cards container scroll is moved when previous/next cards buttons clicked", async () => {
-    const { findByAltText, findByRole } = render(
-      <CardList
-        category={category}
-        params={params}
+  it("Cards container scroll is moved when previous/next cards buttons clicked", () => {
+    const { getByAltText, getByRole } = render(
+      <ContentList
+        conetntType={conetntType}
+        contents={contents}
         contentTitle={contentTitle}
       />
     );
 
     Element.prototype.scrollTo = jest.fn();
 
-    const leftBtn = await findByAltText("previous cards");
-    const rightBtn = await findByAltText("next cards");
-    const cardsContainer = await findByRole("cardsContainer");
+    const leftBtn = getByAltText("previous cards");
+    const rightBtn = getByAltText("next cards");
+    const cardsContainer = getByRole("cardsContainer");
     fireEvent.click(rightBtn);
     expect(cardsContainer.scrollTo).toHaveBeenCalled();
 
