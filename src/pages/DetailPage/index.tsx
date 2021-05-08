d664a43d8fe7dd39ee904f637e0118e035b95205
bbumjun <bumjun7928@gmail.com>
2021-05-08 18:45:23 +0900

    feat: suspense , error boundary 적용

--- src/pages/DetailPage/index.tsx
+++ src/pages/DetailPage/index.tsx
@@ -1,41 +1,36 @@
-import React, { Fragment, useEffect, useState } from "react";
-import { RouteComponentProps } from "react-router-dom";
+import React, { Suspense } from "react";
 import Template from "templates/DetailTemplate";
-import Profile from "components/organizms/Profile";
-import Overview, { IOverview } from "components/organizms/Overview";
-import useContentDetail from "hooks/useContentDetail";
 import { useParams } from "react-router-dom";
 import Loader from "components/atoms/Loader";
-import CastList from "components/organizms/CastList";
-import ContentList from "src/components/organizms/ContentList";
+import ErrorBoundary from "components/atoms/ErrorBoundary";
 
+const ContentList = React.lazy(
+  () => import("components/organizms/ContentList")
+);
+const Profile = React.lazy(() => import("components/organizms/Profile"));
+const Overview = React.lazy(() => import("components/organizms/Overview"));
+const CastList = React.lazy(() => import("components/organizms/CastList"));
 const DetailPage: React.FC = () => {
   const { id, contentType } = useParams<{
     id: string;
     contentType: "movie" | "tv";
   }>();
-  const [loading, data, bgColor, castList, similarList] = useContentDetail({
-    id,
-    contentType,
-  });
   return (
-    <Fragment>
-      {loading ? (
-        <Loader />
-      ) : (
-        <Template profile={<Profile {...data} bgColor={bgColor} />}>
-          <Overview {...data} />
-          <CastList castCards={castList} />
+    <ErrorBoundary>
+      <Suspense fallback={<Loader />}>
+        <Template profile={<Profile contentType={contentType} id={id} />}>
+          <Overview contentType={contentType} id={id} />
+          <CastList contentType={contentType} id={id} />
           <ContentList
             contentTitle="비슷한 작품"
             contentType={contentType}
-            contents={similarList}
+            url={`${contentType}/${id}/similar`}
             wrap={true}
             titleFontSize={1.2}
           />
         </Template>
-      )}
-    </Fragment>
+      </Suspense>
+    </ErrorBoundary>
   );
 };
 
