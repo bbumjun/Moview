b651a22ba9785e0a08eeee2dc7084cb09842e938
bbumjun <bumjun7928@gmail.com>
2021-05-11 23:50:00 +0900

    feat: 슬라이더 재활용을 위한 cardList component 구현

---
+++ src/components/molcules/CardList/index.tsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect, useRef } from "react";
+import * as S from "./style";
+import leftArrow from "images/left-arrow.svg";
+import rightArrow from "images/right-arrow.svg";
+export interface ICardList {
+  contentTitle: string;
+  wrap?: boolean;
+  titleFontSize?: number;
+  items: React.ReactNode;
+}
+const CardList: React.FC<ICardList> = ({
+  contentTitle,
+  wrap = false,
+  titleFontSize = 1.5,
+  items,
+}) => {
+  const containerRef = useRef<HTMLUListElement>();
+  const [containerWidth, setContainerWidth] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPage, setTotalPage] = useState(0);
+
+  useEffect(() => {
+    if (!containerRef.current) {
+      return;
+    }
+    const resizeListener = () => {
+      setContainerWidth(containerRef.current?.offsetWidth);
+      setTotalPage(
+        Math.ceil(
+          containerRef.current?.scrollWidth / containerRef.current?.offsetWidth
+        ) - 1
+      );
+    };
+
+    const observer = new ResizeObserver(resizeListener);
+    observer.observe(containerRef.current);
+    return () => observer.disconnect();
+  }, []);
+  useEffect(() => {
+    containerRef.current.scrollTo({
+      left: currentPage * containerWidth,
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [currentPage, containerWidth]);
+  const handlePageToLeft = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const handlePageToRight = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  return (
+    <S.Wrapper>
+      <S.Title fontSize={titleFontSize} fontWeight={700}>
+        {contentTitle}
+      </S.Title>
+      <S.ContentListContainer
+        ref={containerRef}
+        $wrap={wrap}
+        role="cardsContainer"
+      >
+        {items}
+      </S.ContentListContainer>
+      <S.LeftButton onClick={handlePageToLeft} hidden={currentPage === 0}>
+        <S.StyledIcon src={leftArrow} alt="previous cards" height={1} />
+      </S.LeftButton>
+      <S.RightButton
+        onClick={handlePageToRight}
+        hidden={currentPage === totalPage}
+      >
+        <S.StyledIcon src={rightArrow} alt="next cards" height={1} />
+      </S.RightButton>
+    </S.Wrapper>
+  );
+};
+
+export default CardList;
---
+++ src/components/molcules/CardList/style.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import Icon from "components/atoms/Icon";
+import Button from "components/atoms/Button";
+import Text from "components/atoms/Text";
+export const ContentListContainer = styled.ul<{ $wrap: boolean }>`
+  display: flex;
+  flex-wrap: ${(props) => (props.$wrap ? "wrap" : "nowrap")};
+  padding: 1% 0 3% 0;
+  overflow: hidden;
+  -webkit-overflow-scrolling: touch;
+  scroll-behavior: smooth;
+  @media screen and (max-width: ${({ theme }) => theme.deviceSizes.tablet}) {
+    overflow-x: scroll;
+    overflow-x: ${(props) => (props.$wrap ? "visible" : "scroll")};
+  }
+`;
+export const Wrapper = styled.div`
+  position: relative;
+`;
+export const Title = styled(Text)``;
+export const LeftButton = styled(Button)`
+  position: absolute;
+  border-radius: 100%;
+  box-shadow: -1px 0px 5px gray;
+  left: -1rem;
+  top: 40%;
+  padding: 0.5rem;
+  font-size: 0;
+  @media screen and (max-width: ${({ theme }) => theme.deviceSizes.tablet}) {
+    display: none;
+  }
+`;
+export const RightButton = styled(Button)`
+  position: absolute;
+  border-radius: 100%;
+  box-shadow: 1px 0px 5px gray;
+  font-size: 0;
+  padding: 0.5rem;
+  right: -0.5rem;
+  top: 40%;
+  @media screen and (max-width: ${({ theme }) => theme.deviceSizes.tablet}) {
+    display: none;
+  }
+`;
+export const StyledIcon = styled(Icon)`
+  padding: 0;
+`;
