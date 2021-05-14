d6060483209258907244ac40ede5afa9c37e39e9
bbumjun <bumjun7928@gmail.com>
2021-05-15 04:23:11 +0900

    🐛 recoil value 접근 오류 suspense fallback 감싸서 해결

--- src/components/molcules/FilteredList/index.tsx
+++ src/components/molcules/FilteredList/index.tsx
@@ -1,26 +1,32 @@
-import React from "react";
+import React, { Suspense } from "react";
 import * as S from "./style";
 import Text from "components/atoms/Text";
 import theme from "common/theme";
 import { IContent, IContentList } from "types";
 import FilteredItem from "components/molcules/FilteredItem";
+import ErrorBoundary from "components/atoms/ErrorBoundary";
+import Loader from "components/atoms/Loader";
 export interface FilteredTextListProps {
   items: IContentList;
 }
 const FilteredTextList = ({ items }: FilteredTextListProps) => {
   return (
-    <S.FilteredTextContainer>
-      <Text padding="0.5rem" fontWeight={500} color={theme.colors.red}>
-        연관 검색어
-      </Text>
-      <S.filteredTextList>
-        {items.map((item: IContent) => (
-          <S.filterdTextItem key={item.id}>
-            <FilteredItem data={item} />
-          </S.filterdTextItem>
-        ))}
-      </S.filteredTextList>
-    </S.FilteredTextContainer>
+    <ErrorBoundary>
+      <Suspense fallback={<></>}>
+        <S.FilteredTextContainer>
+          <Text padding="0.5rem" fontWeight={500} color={theme.colors.red}>
+            연관 검색어
+          </Text>
+          <S.filteredTextList>
+            {items.map((item: IContent) => (
+              <S.filterdTextItem key={item.id}>
+                <FilteredItem data={item} />
+              </S.filterdTextItem>
+            ))}
+          </S.filteredTextList>
+        </S.FilteredTextContainer>
+      </Suspense>
+    </ErrorBoundary>
   );
 };
 export default FilteredTextList;
