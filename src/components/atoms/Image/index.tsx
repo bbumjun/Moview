3edd0e3a3981fd5474053acd51589ef999d673b9
bbumjun <bumjun7928@gmail.com>
2021-05-20 16:58:18 +0900

    ✨ 이미지 컴포넌트 레이지 로딩 적용

--- src/components/atoms/Image/index.tsx
+++ src/components/atoms/Image/index.tsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { Ref, useEffect, useRef } from "react";
 import * as S from "./style";
+import useIntersect from "hooks/useIntersect";
 export interface ImageProps {
   src: string;
   alt: string;
   index?: number;
 }
 
-const Image: React.FC<ImageProps> = ({ src, alt }) => (
-  <S.Image src={src} alt={alt}></S.Image>
-);
+const Image: React.FC<ImageProps> = ({ src, alt }) => {
+  const elementRef = useRef<HTMLImageElement>();
+  const IntersectOptions = { freezeOnceVisible: true };
+  const entry = useIntersect(elementRef, IntersectOptions);
+  const isVisible = !!entry?.isIntersecting;
+
+  useEffect(() => {
+    if (isVisible) {
+      elementRef.current.src = src;
+      elementRef.current.alt = alt;
+    }
+  }, [isVisible]);
+  return <S.Image ref={elementRef}></S.Image>;
+};
 
 export default Image;
