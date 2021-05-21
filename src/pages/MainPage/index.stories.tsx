529203466f978807ad3a47c3ffa400c2b09571c0
bbumjun <bumjun7928@gmail.com>
2021-05-21 16:22:25 +0900

    🩹 스토리북 각 페이지 컴포넌트에 header,footer 추가

--- src/pages/MainPage/index.stories.tsx
+++ src/pages/MainPage/index.stories.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Story } from "@storybook/react";
 import MainPage from "./index";
+import Header from "components/organizms/Header";
+import Footer from "components/organizms/Footer";
 
 export default {
   component: MainPage,
   title: "MainPage",
 };
-const Template: Story = () => <MainPage />;
+const Template: Story = () => (
+  <>
+    <Header />
+    <MainPage />
+    <Footer />
+  </>
+);
 export const Default = Template.bind({});
 Default.args = {};
