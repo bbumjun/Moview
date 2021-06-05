92a9d5b8c6ba7cb1699ba342695028377b7b2e6b
bbumjun <bumjun7928@gmail.com>
2021-06-05 16:59:31 +0900

    🎨 Link to type 수정

--- src/components/atoms/Link/style.ts
+++ src/components/atoms/Link/style.ts
@@ -1,4 +1,6 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-export const StyledLink = styled(Link)``;
+export const StyledLink = styled(Link)`
+  color: black;
+`;
 export const Anchor = styled.a``;
