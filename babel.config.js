261a55785599e75d71b5a8a624025ff6b0793be0
bbumjun <bumjun7928@gmail.com>
2021-06-11 01:04:13 +0900

    🔧 babel 타겟 브라우저 설정

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 // babel.config.js
 module.exports = {
   presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
+    [
+      "@babel/preset-env",
+      { targets: { edge: "17", firefox: "60", chrome: "67", safari: "11.1" } },
+    ],
     "@babel/preset-react",
     "@babel/preset-typescript",
   ],
